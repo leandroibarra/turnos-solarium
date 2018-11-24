4b24f718449c1695309f41451c84a26c578ef2f9
Leandro Ibarra <lea14_9@hotmail.com>
2018-11-23 22:24:33 -0300

    Se agrega seeder para roles y parámetros del sistema.

---
+++ database/seeds/SystemParameterTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class SystemParameterTableSeeder extends Seeder
+{
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+		\App\SystemParameter::create([
+			'appointment_minutes' => '10',
+			'appointment_until_days' => '60',
+			'appointment_confirmed_email_subject' => 'Turno Confirmado',
+			'appointment_confirmed_email_body' => '<p>Hola @_NAME_@,</p><p>Su turno ha sido confirmado. Si tiene alguna pregunta o desea reprogramar su turno, contáctenos.</p><p>Hasta pronto.</p><p>Detalles de su turno:</p><p>Fecha: @_DATE_@</p><p>Hora: @_TIME_@</p>',
+			'created_at' => date('Y-m-d H:m:s'),
+			'updated_at' => date('Y-m-d H:m:s')
+		]);
+	}
+}
