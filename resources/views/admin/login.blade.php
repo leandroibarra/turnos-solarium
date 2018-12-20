eb15c166be118e195ad0a56b38f851ec347e0252
Leandro Ibarra <lea14_9@hotmail.com>
2018-12-20 14:56:53 -0300

    Se ajustan centrado de formularios de login y registro.

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center container-center">
-        <div class="col-12 col-lg-8">
+        <div class="col-12 col-sm-8 col-md-6">
             <h2 class="text-center text-uppercase mb-3">{{ __('Administration') }}</h2>
 
             @include('flash::message')
